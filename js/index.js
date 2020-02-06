717250e5b9aed24d1186060f4a110048a8a6e0a5
hh <hh@hhmbp15.local>
2020-02-06 14:49:55 +0000

    3/10 unique events completed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,41 @@
 // Your code goes here
+
+// mouseover
+
+const myNav = document.querySelectorAll('.nav-link');
+// myNav[1].addEventListener('mouseover', (event) => {
+//     event.target.style.color = 'yellow';
+// });
+
+myNav.forEach(element => {
+    element.addEventListener('mouseover', (event) => {
+        event.target.style.color = 'yellow';
+    });
+});
+
+// keydown
+
+const bodyEvent = document.querySelector('body');
+
+bodyEvent.addEventListener('keydown', (event) => {
+    alert('You pressed the secret key!');
+});
+
+// resize
+
+window.addEventListener('resize', (event) => {
+    alert('The window has been resized');
+})
+
+// mouseenter
+
+const contentSection = document.querySelector('.content-section');
+
+contentSection.addEventListener('mouseenter', (event) => {
+    event.target.style.border = '10px solid orange';
+});
+// contentSection.forEach(element => {
+//     element.addEventListener('mouseenter', (event) => {
+//         event.target.style.border = '10px solid orange';
+//     });
+// });
