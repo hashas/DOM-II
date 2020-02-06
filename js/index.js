8525c58ccea43f5137ec739bc1b673b9a0f65a8a
hh <hh@hhmbp15.local>
2020-02-06 22:48:30 +0000

    assignment complete not inc. stretch

--- js/index.js
+++ js/index.js
@@ -13,6 +13,15 @@ myNav.forEach(element => {
     });
 });
 
+// stop nav items from refreshing page using preventDefault()
+
+myNav.forEach(element => {
+    element.addEventListener('click', (event) => {
+        alert('Cannot refresh!')
+        event.preventDefault();
+    });
+});
+
 // keydown
 
 const bodyEvent = document.querySelector('body');
@@ -29,13 +38,54 @@ window.addEventListener('resize', (event) => {
 
 // mouseenter
 
-const contentSection = document.querySelector('.content-section');
+const contentSection = document.querySelectorAll('.content-section');
 
-contentSection.addEventListener('mouseenter', (event) => {
-    event.target.style.border = '10px solid orange';
+contentSection.forEach(element => {
+    element.addEventListener('mouseenter', (event) => {
+        event.target.style.border = '10px solid orange';
+    });
 });
-// contentSection.forEach(element => {
-//     element.addEventListener('mouseenter', (event) => {
-//         event.target.style.border = '10px solid orange';
-//     });
-// });
+
+// mouseleave
+
+contentSection.forEach(element => {
+    element.addEventListener('mouseleave', (event) => {
+        event.target.style.border = 'none';
+    });
+});
+
+// drag
+
+const dragelement = document.querySelectorAll('img');
+
+dragelement.forEach(element => {
+    element.addEventListener('drag', (event) => {
+        alert('STOP! Leave me in my place');
+    });
+});
+
+// stop propagation example
+
+const dest1 = document.querySelector('.destination');
+
+dest1.addEventListener('click', () => {
+    alert('DIV: We are still under construction')
+});
+
+const button1 = document.querySelector('.btn');
+
+button1.addEventListener('click', () => {
+    alert('BUTTON: We are still under construction')
+    event.stopPropagation();
+});
+
+// load
+window.addEventListener('load', () => {
+    alert('This alert showed up on loading the window')
+});
+
+// double click
+
+bodyEvent.addEventListener('dblclick', () => {
+    alert('Secret click at ' + `${event.timeStamp}`);
+});
